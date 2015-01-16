25577508454e26e48e3309889d55b9e559a8dab9
Pat Sabpisal <ssabpisa@me.com>
2015-01-16 04:35:43 -0500

    Testing code flow

--- client_secret.js
+++ client_secret.js
@@ -1,11 +1,36 @@
 /**
  * Handles generation of JWT client secret.
- * - Sign by client_secret_alg_supported or RSA 256
+ * - Sign using client_secret_alg_supported or RSA 256
  * - `ac` key = access code
  */
+
+var jwt = require('jsonwebtoken');
+
+/*
+
+	Generate Client Secret
+	adapted from OADA/oada-id-client-js/src/core.js
+
+*/
+function generateClientSecret(key, issuer, audience, accessCode) {
+    var sec = {
+        ac: accessCode,
+    };
+    var options = {
+        algorithm: 'RS256',
+        audience: audience,
+        issuer: issuer,
+    };
+
+    return jwt.sign(sec, key, options);
+}
+
 
 module.exports = {
   'generate': function(){
 
+  },
+  'sign': function (){
+  	  
   }
 }
--- test.js
+++ test.js
@@ -1,8 +1,27 @@
-var request = require('supertest');  
-describe('Routing', function() {
-  var url = 'http://someurl.com';
+function getEndpoint(type){
+	//for testing purpose
+	var Am = {
+		"authorization_endpoint" : "https://provider.oada-dev.com/auth"
+	}
+	return Am[type];
+}
 
-  before(function(done) {
-      console.log("start test");
-  });
-});
+function expand_param(dict){
+	var str = [];
+	for(var key in dict){
+		str.push(key + "=" + encodeURIComponent(dict[key]));
+	}
+	return str.join("&");
+}
+
+function getAccessCode() {
+    var auth = getAuthorizationEndpoint("authorization_endpoint");
+    var param = {
+    	"response_type" : "code",
+    	"client_id": "3klaxu838akahf38acucaix73@identity.oada-dev.com",
+    	"state" : "xyz",
+    	"redirect_uri": "https://client.oada-dev.com/redirect",
+    	"scope": "bookmarks.fields"
+    }
+    var url = auth + "/" + "?" + expand_param(param);
+}
