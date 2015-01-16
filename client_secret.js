349e1737b75ddf38a5c183aec31cd4ee2db074eb
Pat Sabpisal <ssabpisa@me.com>
2015-01-16 02:21:11 -0500

    Trying to generate JWT

---
+++ client_secret.js
@@ -0,0 +1,11 @@
+/**
+ * Handles generation of JWT client secret.
+ * - Sign by client_secret_alg_supported or RSA 256
+ * - `ac` key = access code
+ */
+
+module.exports = {
+  'generate': function(){
+
+  }
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');  
+describe('Routing', function() {
+  var url = 'http://someurl.com';
+
+  before(function(done) {
+      console.log("start test");
+  });
+});
