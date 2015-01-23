7aeff115012e9ef18c587a5dc63ba1888d1ad1e1
Pat Sabpisal <ssabpisa@me.com>
2015-01-23 13:52:13 -0500

    Example mocha and chai

--- mocha_test.js
+++ mocha_test.js
@@ -1,13 +1,37 @@
-var assert = require("assert")
 var request = require('supertest');
+var chai = require('chai');
+chai.use(require('chai-json-schema'));
+var assert = chai.assert;
+var should = chai.should();
+
 request = request('https://identity.oada-dev.com');
 
-describe('GET oada-configuration', function(){
-  it('respond with oada-configuration document', function(done){
+describe('Pre-requisite .well_known/oada-configuration', function(){
+  it('should respond with oada-configuration document', function(done){
     request
       .get('/.well-known/oada-configuration')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, done);
-  })
-})
+      .expect(200)
+      .end(function(err,res){
+      	should.not.exist(err);
+      	assert.jsonSchema(JSON.parse(res.text), require("./schema/oada_configuration.json"));
+      	done()
+      });
+  });
+});
+
+describe('Pre-requisite .well_known/oada-client-discovery', function(){
+  it('should respond with oada-client-discovery document', function(done){
+    request
+      .get('/.well-known/oada-client-discovery')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err,res){
+      	should.not.exist(err);
+      	assert.jsonSchema(JSON.parse(res.text), require("./schema/oada_client_discovery.json"));
+      	done()
+      });
+  });
+});
